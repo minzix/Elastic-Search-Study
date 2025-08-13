5b511f3e7a422a6ae4cf17b35cdd92dd507a58ea
김민지 <minzikx@sookmyung.ac.kr>
2025-08-13 17:32:51 +0900

    Feat: createUser 메서드 POST controller에 추가

--- src/main/java/study/elastic/controller/UserController.java
+++ src/main/java/study/elastic/controller/UserController.java
@@ -1,8 +1,12 @@
 package study.elastic.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.elastic.domain.UserDocument;
+import study.elastic.dto.UserCreateRequestDTO;
 import study.elastic.repository.UserDocumentRepository;
 
 @RestController
@@ -11,5 +15,14 @@ import study.elastic.repository.UserDocumentRepository;
 public class UserController {
     private UserDocumentRepository userDocumentRepository;
 
-
+    @PostMapping
+    public UserDocument createUser(@RequestBody UserCreateRequestDTO requestDTO) {
+        UserDocument user = new UserDocument(
+                requestDTO.getId(),
+                requestDTO.getName(),
+                requestDTO.getAge(),
+                requestDTO.getIsActive()
+        );
+        return userDocumentRepository.save(user);
+    }
 }
