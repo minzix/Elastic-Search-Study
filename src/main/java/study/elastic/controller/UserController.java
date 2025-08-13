8ec3862f347e8b69369da013f739828de64a78f2
김민지 <minzikx@sookmyung.ac.kr>
2025-08-13 18:12:15 +0900

    Feat: userController에 deleteUser 추가

--- src/main/java/study/elastic/controller/UserController.java
+++ src/main/java/study/elastic/controller/UserController.java
@@ -50,4 +50,11 @@ public class UserController {
         existingUser.setIsActive(requestDTO.getIsActive());
         return userDocumentRepository.save(existingUser);
     };
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        UserDocument user = userDocumentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("사용자가 존재하지 않음."));
+        userDocumentRepository.delete(user);
+    }
 }
