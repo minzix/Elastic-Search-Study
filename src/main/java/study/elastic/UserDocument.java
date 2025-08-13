cbcc3f09a35cf6676ca5898a18c5b0d80249414c
김민지 <minzikx@sookmyung.ac.kr>
2025-08-13 16:55:54 +0900

    Feat: UserDocument 생성

---
+++ src/main/java/study/elastic/UserDocument.java
@@ -0,0 +1,21 @@
+package study.elastic;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "users")
+public class UserDocument {
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Keyword)
+    private String name;
+
+    @Field(type = FieldType.Long)
+    private Long age;
+
+    @Field(type = FieldType.Boolean)
+    private Boolean isActive;
+}
