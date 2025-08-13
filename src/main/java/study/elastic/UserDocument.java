6720f72cf3c6604ac5c029b4bce1988942bd5c39
김민지 <minzikx@sookmyung.ac.kr>
2025-08-13 16:57:00 +0900

    Feat: UserDocument의 Getter/Setter 추가

--- src/main/java/study/elastic/UserDocument.java
+++ src/main/java/study/elastic/UserDocument.java
@@ -25,4 +25,36 @@ public class UserDocument {
         this.age = age;
         this.isActive = isActive;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
